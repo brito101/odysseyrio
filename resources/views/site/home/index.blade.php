f04b77c0392b6e58a956f2d44f9260b59ffd2c6a
brito101 <rodrigobrito101@hotmail.com>
2024-02-07 19:22:05 -0300

    mod: adjust Google lighthouse

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -24,7 +24,7 @@
                         @for ($i = 1; $i <= 8; $i++)
                             <div class="carousel-item {{ $i == 1 ? 'active' : '' }}">
                                 <img src="{{ asset('img/slide/slide-' . $i . '.webp') }}" class="d-block w-100"
-                                    alt="...">
+                                    alt="..." width="756" height="425">
                             </div>
                         @endfor
                     </div>
